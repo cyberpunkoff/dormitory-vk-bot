a265860fe31b14d66bfee047376046c248fd178d
Maksim Ustinov <ustiinovmp@gmail.com>
2024-05-01 01:43:15 +0300

    feat: added migrations, configured database

---
+++ migrations/init_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS student
+(
+    student_id      BIGINT      NOT NULL,
+
+    UNIQUE (student_id),
+    PRIMARY KEY (student_id)
+);
+
+CREATE TABLE IF NOT EXISTS employee
+(
+    employee_id       BIGINT        GENERATED ALWAYS AS IDENTITY,
+    employee_name     VARCHAR(255)  NOT NULL,
+    description       TEXT          NOT NULL,
+
+    PRIMARY KEY(employee_id)
+);
